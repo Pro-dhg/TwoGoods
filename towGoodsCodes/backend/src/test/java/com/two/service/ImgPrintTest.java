c6ac320f8af951b438abe60b97ab05ba40374fc6
dhg <hg.duan@yamu.com>
2022-10-27 16:43:02 +0800

    添加单元测试

--- towGoodsCodes/backend/src/test/java/com/two/service/ImgPrintTest.java
+++ towGoodsCodes/backend/src/test/java/com/two/service/ImgPrintTest.java
@@ -1,9 +1,9 @@
-package com.yamu.data.sample.service.rpt;
+package com.two.service;
 
 
 import org.junit.Test;
 
-public class BusinessTests {
+public class ImgPrintTest {
     private static String HELLO_WORLD = "く__,.ヘヽ.　　　　/　,ー､ 〉\n　　　＼ ', !-─‐-i　/　/´\n　　　 　 ／｀ｰ'　　　 L/／｀ヽ､\n　　 　 /　 ／,　 /|　 ,　 ,　　　 ',\n　　　ｲ 　/ /-‐/　ｉ　L_ ﾊ ヽ!　 i\n　　　 ﾚ ﾍ 7ｲ｀ﾄ　 ﾚ'ｧ-ﾄ､!ハ|　 |\n　　　　 !,/7 '✪'　　 ´i✪ﾊiソ| 　 |　　　\n　　　　 |.从\"　　_　　 ,,,, / |./ 　 |\n　　　　 ﾚ'| i＞.､,,__　_,.イ / 　.i 　|\n　　　 ﾚ'| | / k_７_/ﾚ'ヽ,　ﾊ.　|\n　　　 | |/i 〈|/　 i　,.ﾍ |　i　|\n　　　.|/ /　ｉ： 　 ﾍ!　　＼　|\n　　　 　 　 kヽ>､ﾊ 　 _,.ﾍ､ 　 /､!\n　　　 !'〈//｀Ｔ´', ＼ ｀'7'ｰr'\n　　　 ﾚ'ヽL__|___i,___,ンﾚ|ノ\n　　　 　　　ﾄ-,/　|___./\n　　　 　　　'ｰ'　　!_,.:";
 
     @Test
